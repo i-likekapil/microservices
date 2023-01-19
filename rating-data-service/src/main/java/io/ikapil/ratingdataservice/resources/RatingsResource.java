5e81f7a26df60aa9301f71852c8d9a1cbe698e81
Kapil Kaushik <kapil.kaushik@cavisson.com>
2023-01-19 18:16:31 +0530

    rating-data-service create simple route

---
+++ rating-data-service/src/main/java/io/ikapil/ratingdataservice/resources/RatingsResource.java
@@ -0,0 +1,30 @@
+package io.ikapil.ratingdataservice.resources;
+
+import io.ikapil.ratingdataservice.model.Rating;
+import io.ikapil.ratingdataservice.model.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Kapil Kaushik on 19/01/23
+ * @project movie-catalog-service
+ */
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+}
