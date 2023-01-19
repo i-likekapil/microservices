b035cf8963dcda37627933d6f5e31bd0a9c384c8
Kapil Kaushik <kapil.kaushik@cavisson.com>
2023-01-19 18:48:56 +0530

    complete code

---
+++ movie-catalog-service/src/main/java/io/ikapil/moviecatalogservice/models/Rating.java
@@ -0,0 +1,35 @@
+package io.ikapil.moviecatalogservice.models;
+
+/**
+ * @author Kapil Kaushik on 19/01/23
+ * @project movie-catalog-service
+ */
+
+public class Rating {
+    private String movieId;
+    private int rating;
+
+    public Rating() {
+    }
+
+    public Rating(String movieId, int rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
---
+++ movie-catalog-service/src/main/java/io/ikapil/moviecatalogservice/resources/CatalogResource.java
@@ -0,0 +1,40 @@
+package io.ikapil.moviecatalogservice.resources;
+
+import io.ikapil.moviecatalogservice.models.CatalogItem;
+import io.ikapil.moviecatalogservice.models.Movie;
+import io.ikapil.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Kapil Kaushik on 19/01/23
+ * @project movie-catalog-service
+ */
+
+@RestController
+@RequestMapping("/catalog")
+public class CatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+
+        return userRating.getRatings().stream()
+                .map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+    }
+}
